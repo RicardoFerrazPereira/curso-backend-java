4316e522d6cc0ae662831bcdec0f813ef4cb578f
RicardoFerraz <sourvivens@gmail.com>
2022-07-23 20:58:09 -0300

    Service layer, component registration

--- src/main/java/com/sourproject/course/resource/ClientResource.java
+++ src/main/java/com/sourproject/course/resource/ClientResource.java
@@ -1,20 +1,34 @@
 package com.sourproject.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sourproject.course.entities.Client;
+import com.sourproject.course.services.ClientService;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
-	public ResponseEntity<Client> findAll() {
-		Client u = new Client(1L, "Maria", "maria@gmail.com", "1234567", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> findById(@PathVariable Long id) {
+		Client obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
