93990c2b78f2ba3dbc753383a4a1f6bbe0c0b374
RicardoFerraz <sourvivens@gmail.com>
2022-07-24 17:49:47 -0300

    Exception handling - update

--- src/main/java/com/sourproject/course/services/ClienteService.java
+++ src/main/java/com/sourproject/course/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.sourproject.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +47,13 @@ public class ClienteService {
 	
 	@SuppressWarnings("deprecation")
 	public Cliente update(Long id, Cliente obj) {
-		Cliente entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Cliente entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(Cliente entity, Cliente obj) {
